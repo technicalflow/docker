1292eb6fc36209a5639ef5c1a3e1bbeb738ae84d
Marek <51706013+technicalflow@users.noreply.github.com>
2021-12-29 13:00:00 +0100

    small changes

--- code-on-lan/root/script.sh
+++ code-on-lan/root/script.sh
@@ -84,6 +84,13 @@ echo "vsadmin ALL=(ALL) NOPASSWD:ALL" | tee /etc/sudoers.d/vsadmin
 chown -R vsadmin:vsadmin /config
 chown -R vsadmin:vsadmin /home/vsadmin
 
+cat << EOFsettings > /config/data/User/settings.json 
+{
+    "workbench.colorTheme": "Visual Studio Dark",
+    "terminal.integrated.gpuAcceleration": "off",
+}
+EOFsettings
+
 echo "**** Files ****" 
 touch /home/vsadmin/.bashrc
 echo "
