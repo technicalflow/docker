7bf190acc598508115361ff87df3371b6315147b
Marek <techfellow@outlook.com>
2022-01-13 08:14:44 +0100

    small changes

--- webapp/my-entrypoint.sh
+++ webapp/my-entrypoint.sh
@@ -20,7 +20,6 @@ cat << EOFhtml > /usr/share/nginx/html/index.html
 <img style="padding: 20px;" src="https://www.docker.com/sites/default/files/social/docker_facebook_share.png" alt="Blue container"><br>
 <h2>Hostname: </h2>
 <h2>Distribution: </h2>
-<h2>Nginx Version: </h2>
 <h2>Container IP: </h2><br>
 </body></html>
 EOFhtml
@@ -30,7 +29,7 @@ DISTRO=$(cat /etc/os-release | grep PRETTY | cut -c 13-50)
 DIST=$(echo $DISTRO | sed 's/\// /' | cut -c 1-20)
 IP=$(awk '/32 host/ { print f } {f=$2}' /proc/net/fib_trie | sort | uniq | grep -v 127 | sed ':a; N; $!ba; s/\n/ /g')
 # NGINX_VERSION=$(/usr/sbin/nginx -v)
-NGINX_ALPINE=$(apk info -q nginx  | grep nginx | head -q -c 15)
+# NGINX_ALPINE=$(apk info -q nginx  | grep nginx | head -q -c 15)
 # NGINX_DEBIAN=$(apt info nginx | grep Version)
 
 #ip a | grep inet |  grep -v 127 | cut -c 10-22 | tail -n 3 >> /tmp/IP
@@ -39,7 +38,7 @@ NGINX_ALPINE=$(apk info -q nginx  | grep nginx | head -q -c 15)
 
 sed -i 's/<h2>Hostname:.*/<h2>Hostname: '"$HOSTNAME"'<\/h2> /' /usr/share/nginx/html/index.html
 sed -i 's/<h2>Distribution:.*/<h2>Distribution: '"$DIST"'<\/h2> /' /usr/share/nginx/html/index.html
-sed -i 's/<h2>Nginx Version:.*/<h2>Nginx Version: '"$NGINX_ALPINE"'<\/h2> /' /usr/share/nginx/html/index.html
+# sed -i 's/<h2>Nginx Version:.*/<h2>Nginx Version: '"$NGINX_ALPINE"'<\/h2> /' /usr/share/nginx/html/index.html
 sed -i 's/<h2>Container IP:.*/<h2>Container IP: '"$IP"'<\/h2> /' /usr/share/nginx/html/index.html
 
 # echo DONE
